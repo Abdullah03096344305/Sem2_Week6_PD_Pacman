114d1750f3b4cd0d5d403d54eada638f8f4a7d11
Abdullah03096344305 <abdullah03096344305@gmail.com>
2023-06-02 14:01:47 +0500

    Add project files.

---
+++ Week6_PD_Pacman/BL/Pacman.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6_PD_Pacman.BL
+{
+    class Cell
+    {
+        public char Value { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Cell(char value, int x, int y)
+        {
+            Value = value;
+            X = x;
+            Y = y;
+        }
+
+        public bool IsPacmanPresent()
+        {
+            return Value == 'P';
+        }
+
+        public bool IsGhostPresent()
+        {
+            return Value == 'G';
+        }
+    }
+
+    class Grid
+    {
+        public List<List<Cell>> Cells { get; set; }
+
+        public Grid(int rowSize, int columnSize, string path)
+        {
+            Cells = new List<List<Cell>>(rowSize);
+            for (int i = 0; i < rowSize; i++)
+            {
+                var row = new List<Cell>();
+                for (int j = 0; j < columnSize; j++)
+                {
+                    
+                }
+                Cells.Add(row);
+            }
+        }
+
+        public Cell GetLeftCell(Cell cell)
+        {
+            if (cell.X > 0)
+            {
+                return Cells[cell.Y][cell.X - 1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Cell GetRightCell(Cell cell)
+        {
+            if (cell.X < Cells[cell.Y].Count - 1)
+            {
+                return Cells[cell.Y][cell.X + 1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Cell GetUpCell(Cell cell)
+        {
+            if (cell.Y > 0)
+            {
+                return Cells[cell.Y - 1][cell.X];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Cell GetDownCell(Cell cell)
+        {
+            if (cell.Y < Cells.Count - 1)
+            {
+                return Cells[cell.Y + 1][cell.X];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Cell FindPacman()
+        {
+            foreach (var row in Cells)
+            {
+                foreach (var cell in row)
+                {
+                    if (cell.IsPacmanPresent())
+                    {
+                        return cell;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public Cell FindGhost(char character)
+        {
+            foreach (var row in Cells)
+            {
+                foreach (var cell in row)
+                {
+                    if (cell.IsGhostPresent() && cell.Value == character)
+                    {
+                        return cell;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void Draw()
+        {
+            foreach (var row in Cells)
+            {
+                foreach (var cell in row)
+                {
+                    Console.Write(cell.Value);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+    /*===========================================================================*/
+    class Ghost
+    {
+        public int X{ get; set;}
+        public int Y { get; set; }
+        public int Speed { get; set; }
+        public Random random = new Random();
+
+        public Ghost(int x, int y, int speed)
+        {
+            X = x;
+            Y = y;
+            Speed = speed;
+        }
+
+        public void Move()
+        {
+            int direction = random.Next(4);
+            switch (direction)
+            {
+                case 0:
+                    X += Speed;
+                    break;
+                case 1:
+                    X -= Speed;
+                    break;
+                case 2:
+                    Y += Speed;
+                    break;
+                case 3:
+                    Y -= Speed;
+                    break;
+            }
+        }
+
+        public void Draw()
+        {
+            Console.Write("G");
+        }
+    }
+
+    class Pacman
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Pacman(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Move(char direction)
+        {
+            switch (direction)
+            {
+                case 'W':
+                    Y -= 1;
+                    break;
+                case 'A':
+                    X -= 1;
+                    break;
+                case 'S':
+                    Y += 1;
+                    break;
+                case 'D':
+                    X += 1;
+                    break;
+            }
+        }
+    }
+
+}
---
+++ Week6_PD_Pacman/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Week6_PD_Pacman.BL;
+using System.Threading.Tasks;
+
+namespace Week6_PD_Pacman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a maze
+            Grid maze = new Grid(20, 20, "maze.txt");
+
+            // Create a pacman
+            Pacman pacman = new Pacman(10, 10);
+
+            // Create ghosts
+            List<Ghost> ghosts = new List<Ghost>();
+            for (int i = 0; i < 4; i++)
+            {
+                ghosts.Add(new Ghost(i * 10, i * 10, 1));
+            }
+
+            // Start the game loop
+            while (true)
+            {
+                // Clear the screen
+                Console.Clear();
+
+                // Draw the maze
+                maze.Draw();
+
+                // Draw the pacman
+                Console.Write("P");
+
+                // Draw the ghosts
+                foreach (var ghost in ghosts)
+                {
+                    Console.Write("G");
+                }
+
+                // Get the user input
+                Console.ReadKey();
+
+                // Move the pacman
+                char direction = Console.ReadKey().KeyChar;
+                pacman.Move(direction);
+
+                // Move the ghosts
+                foreach (var ghost in ghosts)
+                {
+                    ghost.Move();
+                }
+
+                // Check if the pacman has reached the exit
+                if (maze.FindPacman().Value == 'E')
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                }
+
+                // Check if the pacman has collided with a ghost
+                foreach (var ghost in ghosts)
+                {
+                    if (pacman.X == ghost.X && pacman.Y == ghost.Y)
+                    {
+                        Console.WriteLine("You lose!");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+ 
+
